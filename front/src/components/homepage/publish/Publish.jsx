5c127b37a881ad86b5983efbd05c201a83aaad46
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-26 15:52:39 +0200

    publish component (static)

--- front/src/components/homepage/publish/Publish.jsx
+++ front/src/components/homepage/publish/Publish.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineCamera } from 'react-icons/ai';
+
 
 const Publish = () => {
   return (
-    <div>Publish</div>
+    <section>
+      <div className="publish__container">
+          <AiOutlineUser className='publish__img'/>
+        <div className="publish__content">
+          <input type="text" name="publish-content" id="content" placeholder='Quoi de neuf ?' />
+          <AiOutlineCamera  className='publish__content-camera'/>
+        </div>
+        <button className="btn"> Publier</button>
+      </div>
+    </section>
   )
 }
 
