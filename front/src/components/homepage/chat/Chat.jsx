93042118dca2e1a945ce82c8c999e604bb32a6ca
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-27 18:01:16 +0200

    edit publication component and create chat component

--- front/src/components/homepage/chat/Chat.jsx
+++ front/src/components/homepage/chat/Chat.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { FaUsers } from 'react-icons/fa';
+import { AiOutlineUser } from 'react-icons/ai';
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <section className='chat'>
+      <div className="chat__container">
+        <div className="chat__header">
+          <FaUsers  className='chat__header-icon'/>
+          <h2> Chat général</h2>
+        </div>
+        <div className="chat__content">
+          <div className="chat__content-id">
+            <AiOutlineUser className='chat__content-id--icon'/>
+            <h3>User</h3>
+          </div>
+          <div className="chat__content-text">
+            <p>Hello moi c'est User !</p>
+          </div>
+        </div>
+        <div className="chat__content">
+          <div className="chat__content-id">
+            <AiOutlineUser className='chat__content-id--icon'/>
+            <h3>User 2</h3>
+          </div>
+          <div className="chat__content-text">
+            <p>Punaise on a le même nom !</p>
+          </div>
+        </div>
+      </div>
+      <div className="chat__new">
+          <input type="text" name="chat-new-message" id="chat" placeholder='Nouveau message...' />
+        </div>
+    </section>
   )
 }
 
--- front/src/components/homepage/publication/Publication.jsx
+++ front/src/components/homepage/publication/Publication.jsx
@@ -7,7 +7,8 @@ import Bob from '../../../assets/Bob-leponge.png'
 const Publication = () => {
   return (
     <section>
-      <div className="publication__container">
+    <div className="publication">
+    <div className="publication__container">
         <div className="publication__author">
           <AiOutlineUser className='publication__author-img'/>
           <div className="publication__author-text">
@@ -30,6 +31,29 @@ const Publication = () => {
           </button>
         </div>
       </div>
+      <div className="publication__container">
+        <div className="publication__author">
+          <AiOutlineUser className='publication__author-img'/>
+          <div className="publication__author-text">
+            <h2>Nowfeel SAFI</h2>
+            <p> Il y a 22 minutes</p>
+          </div>
+        </div>
+        <div className="publication__content">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ducimus cumque ex corrupti omnis officia eaque dignissimos odio itaque quam.</p>
+        </div>
+        <div className="publication__reaction">
+          <button className="btn publication__reaction-like">
+            <BiLike className='publication__reaction-like-icon'/>
+            <h3> J'AIME </h3>
+          </button>
+          <button className="btn publication__reaction-comment">
+            <BiComment className='publication__reaction-comment-icon'/>
+            <h3> COMMENTER</h3>
+          </button>
+        </div>
+      </div>
+    </div>
     </section>
   )
 }
