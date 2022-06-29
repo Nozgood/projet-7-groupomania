526e35eb7ef4d9b8fe24ac60a05a60e9acd755a4
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-29 15:34:40 +0200

    start to display profile page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './index.css';
 import Homepage from './components/homepage/Homepage';
 import Login from './components/login/Login';
+import Profile from './components/profile/Profile';
 
 const App = () => {
     return (
         <>
-            <Login />
+            <Profile />
         </>
     )
 }
--- front/src/components/profile/Profile.jsx
+++ front/src/components/profile/Profile.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Header from '../homepage/header/Header'
+import zoro from '../../assets/roronoa-zoro.jpeg'
+import zoroProfile from '../../assets/zoro-profile.jpeg'
+import { RiToolsFill } from 'react-icons/ri'
 
 const Profile = () => {
   return (
-    <div id='profile'>Profile</div>
+    <>
+      <Header />
+      <main>
+        <div className="profile__container">
+          <div className="profile__cover">
+            <img src={ zoro } alt="couverture de roronoa zoro" />
+          </div>
+          <div className="profile__infos">
+            <div className="profile__infos-img">
+              <img src={zoroProfile} alt="profil zoro" />
+              <div className="profile__infos-text">
+                <h1 className="profile__infos-text-name">Roronoa Zoro</h1>
+              </div>
+            </div>
+            <div className="profile__modification">
+                <button type="submit">
+                  <RiToolsFill className='profile__modification-icon' />
+                  <p className="profile__modification-text"> Modifier le profil</p>
+                </button>
+            </div>
+          </div>
+          <div className="profile__publications">
+            <article className="profile__publications-article">
+              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
+            </article>
+            <article className="profile__publications-article">
+              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
+            </article>
+            <article className="profile__publications-article">
+              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
+            </article>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
