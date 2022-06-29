d5869f7f8ad019424cb88aa5086046648bb212c2
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-29 17:11:12 +0200

    profile page (design degueu)

--- front/src/components/login/Login.jsx
+++ front/src/components/login/Login.jsx
@@ -4,26 +4,26 @@ import {AiFillEyeInvisible} from 'react-icons/ai';
 
 const Login = () => {
     return (
-        <main>
-    <div className="login__img">
-        <img src={logo} alt="groupomania" />
-    </div>
-    <h1 className="login__title">
-        Groupomania, to Get In Touch With Your Coworkers !
-    </h1>
-    <section className='login__section'>
-    <form className='login__form' action="">
-        <input type="email" name="mail" id="login-mail" placeholder='Adresse Mail' />
-        <div className="login__password">
-            <input type="password" name="password" id="login-mdp" placeholder='Mot de passe ...' />
-            <AiFillEyeInvisible  className='login__password-icon'/>
-        </div>
-    <input type="submit" value="SE CONNECTER" id='connect'/>
-    </form>
-    <a className='login__passowrd--forget' href="#"> Mot de passe oublié ?</a>
-    <button className="login__signup"> CRÉER UN COMPTE</button>
-    </section>
-    </main>
+        <main className='login'>
+            <div className="login__img">
+                <img src={logo} alt="groupomania" />
+            </div>
+            <h1 className="login__title">
+                Groupomania, to Get In Touch With Your Coworkers !
+            </h1>
+            <section className='login__section'>
+                <form className='login__form' action="">
+                    <input type="email" name="mail" id="login-mail" placeholder='Adresse Mail' />
+                    <div className="login__password">
+                        <input type="password" name="password" id="login-mdp" placeholder='Mot de passe ...' />
+                        <AiFillEyeInvisible  className='login__password-icon'/>
+                    </div>
+                    <input type="submit" value="SE CONNECTER" id='connect'/>
+                </form>
+                <a className='login__passowrd--forget' href="#"> Mot de passe oublié ?</a>
+                <button className="login__signup"> CRÉER UN COMPTE</button>
+            </section>
+        </main>
     )
 }
 
--- front/src/components/profile/Profile.jsx
+++ front/src/components/profile/Profile.jsx
@@ -3,6 +3,7 @@ import Header from '../homepage/header/Header'
 import zoro from '../../assets/roronoa-zoro.jpeg'
 import zoroProfile from '../../assets/zoro-profile.jpeg'
 import { RiToolsFill } from 'react-icons/ri'
+import Publication from '../homepage/publication/Publication';
 
 const Profile = () => {
   return (
@@ -14,31 +15,25 @@ const Profile = () => {
             <img src={ zoro } alt="couverture de roronoa zoro" />
           </div>
           <div className="profile__infos">
+          <div className="profile__infos-visual">
             <div className="profile__infos-img">
               <img src={zoroProfile} alt="profil zoro" />
+              </div>
               <div className="profile__infos-text">
                 <h1 className="profile__infos-text-name">Roronoa Zoro</h1>
               </div>
-            </div>
-            <div className="profile__modification">
-                <button type="submit">
-                  <RiToolsFill className='profile__modification-icon' />
-                  <p className="profile__modification-text"> Modifier le profil</p>
-                </button>
-            </div>
+          </div>
+          <div className="profile__infos--modification">
+            <button type="submit">
+              <RiToolsFill className='profile__infos--modification-icon' />
+              <p className="profile__modification-text"> Modifier le profil</p>
+            </button>
+          </div>
+          </div>
           </div>
           <div className="profile__publications">
-            <article className="profile__publications-article">
-              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
-            </article>
-            <article className="profile__publications-article">
-              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
-            </article>
-            <article className="profile__publications-article">
-              <small> Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam autem praesentium consequatur cupiditate animi nihil blanditiis minus soluta esse?</small>
-            </article>
+            <Publication />
           </div>
-        </div>
       </main>
     </>
   )
